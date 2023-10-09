360d21716cb06277fd6542d45cc055e5f83451db
vijayalakshmipushpendra <vijayalakshmi1812@gmail.com>
2023-10-09 15:54:39 +0530

    Initial commit

---
+++ maven_appp/src/main/java/com/maven/demo1/maven_appp/App.java
@@ -0,0 +1,13 @@
+package com.maven.demo1.maven_appp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
